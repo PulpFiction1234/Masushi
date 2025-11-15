7c05896b603878ac7f7d29a9a183b174b0c5af22
PulpFiction1234 <rafaelalbertobenguria@gmail.com>
2025-11-15 01:57:51 -0300

    icono

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -19,6 +19,7 @@ export default function Document() {
         {/* Favicons */}
         <link rel="icon" type="image/x-icon" href="/favicon.ico" />
         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="manifest" href="/manifest.json" />
